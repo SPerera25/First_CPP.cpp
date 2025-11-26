c9797da3201727fc5f0c37b23a63c6f8d7adf67f
S Perera <sperera1922@gmail.com>
2025-11-26 16:59:01 +0530

    Add basic C++ program to demonstrate line breaks

---
+++ Next line.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main () {
+  cout<<"First line \n"; //Just use '\n' to go to the next line.
+  cout<<"Second line "<<endl; //Or you can use 'endl'.
+  return 0;
+}
