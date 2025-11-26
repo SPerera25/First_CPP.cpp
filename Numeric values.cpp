416732b1b02b2bc286ce81dc0afd9057b35942d6
S Perera <sperera1922@gmail.com>
2025-11-26 17:09:40 +0530

    Implement basic output for integers and strings
    Demonstrates output of numeric values as both integer and string.

---
+++ Numeric values.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main () {
+  cout<<4<<endl; //In here 4 reads as a integer.
+  cout<<"4 \n"; // Here it reads as a string.Because 4 is inside the "".
+  return o;
+}
