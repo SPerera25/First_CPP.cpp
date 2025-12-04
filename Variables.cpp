acec7bad960c22a43edad85186340dda9f129116
S Perera <sperera1922@gmail.com>
2025-12-04 09:11:12 +0530

    Implement variable declarations and output in main
    Added variable declarations and output statements.

---
+++ Variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+  int x; //This is a declaration statement.
+  x = 10; //This is a assignment statement.
+  float y = 10.94; //This is a statement that have declaration and assignment together.
+  cout << x << endl;
+  cout << y << endl;
+  return 0;
+}
