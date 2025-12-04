cec6bf4bd3be65783aa628ec50f676103214e8eb
S Perera <sperera1922@gmail.com>
2025-12-04 09:15:40 +0530

    Add variable z with brace initialization
    Introduce variable z with brace initialization.

--- Variables.cpp
+++ Variables.cpp
@@ -4,7 +4,9 @@ int main (){
   int x; //This is a declaration statement.
   x = 10; //This is a assignment statement.
   float y = 10.94; //This is a statement that have declaration and assignment together.
+  int z{20}; //This is not common, but use.
   cout << x << endl;
   cout << y << endl;
+  cout << z << endl;
   return 0;
 }
