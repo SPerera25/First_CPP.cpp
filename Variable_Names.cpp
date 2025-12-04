0e8d02094736d2fff32a019bf63d2c2206c56e3e
S Perera <sperera1922@gmail.com>
2025-12-04 10:20:50 +0530

    Add initial file structure for Variable Names.cpp

---
+++ Variable_Names.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int num;
+    cout<<"Enter an integer: ";
+    cin>>num;
+    /*Here we declare a variable with a valid name.
+    We get the value from the user and store it in the variable 'num'.*/
+    return 0;
+}
